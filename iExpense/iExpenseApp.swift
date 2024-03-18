6cc3e0e709f6cb49d610cdd8bbb30e70e6ddb2ac
koonzwg <koonzwg@gmail.com>
2024-03-18 13:53:04 -0500

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by William Koonz on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
